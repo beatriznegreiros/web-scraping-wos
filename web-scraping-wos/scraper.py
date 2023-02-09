1900f6f039a6aa4e64cba074bc36ad3c0e806013
Beatriz Negreiros (IWS) <beatriznegreiros@outlook.com>
2023-02-09 09:22:04 +0100

    bypassed remind later cookie

--- web-scraping-wos/scraper.py
+++ web-scraping-wos/scraper.py
@@ -3,26 +3,33 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
+driver.maximize_window()
 driver.get('https://www.webofscience.com/wos/woscc/summary/ff7d7f65-1ac6-4213-b788-f3caf673d7fd-6c336e02/relevance/1')
-# mycookies = driver.get_cookies()
-# print(mycookies)
-# print(len(mycookies))
-# driver.delete_all_cookies()
+
 time.sleep(3)
 consent_button = driver.find_element(By.ID, value='onetrust-accept-btn-handler')
 consent_button.click()
-time.sleep(10)
+
 try:
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'pendo-button-f8b7283d')))
     remind_button = driver.find_element(By.ID, value='pendo-button-f8b7283d')
     remind_button.click()
-finally:
-    element = 'show-more.show-more-text'
-    # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, element)))
-    for btn in driver.find_elements(By.CLASS_NAME, element):
-        btn.click()
+except TimeoutException:
+    pass
+
+element = 'show-more.show-more-text'
+btns = driver.find_elements(By.CSS_SELECTOR, element)
+print(btns)
+for btn in btns:
+    btn.click()
+    print(btn)
+    time.sleep(3)
+
 #
 # html = driver.page_source
 #
